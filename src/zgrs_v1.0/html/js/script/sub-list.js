28d2f1ef655c0f5a5a937603472c5866c9ef5980
jackbuy <2538362801@qq.com>
2018-05-30 09:20:31 +0800

    新增页面

--- src/zgrs_v1.0/html/js/script/sub-list.js
+++ src/zgrs_v1.0/html/js/script/sub-list.js
@@ -1,3 +1,3 @@
-/*!all-2018-05-24 */
+/*!all-2018-05-30 */
 
 $(function(){var a=$(".sub-list").find("li");a.eq(2).css({width:"50%"}),a.eq(3).css({width:"50%"}),a.eq(8).css({width:"50%"}),a.eq(9).css({width:"50%"}),a.each(function(){$(this).find("a").hover(function(){$(this).find("div").css({bottom:0})},function(){$(this).find("div").css({bottom:"-60px"})})})});
